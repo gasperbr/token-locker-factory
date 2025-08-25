f809a489abc136a14e3cf8f786961dae05488e3a
Moody Salem <moody.salem@gmail.com>
2025-08-25 16:29:01 -0400

    remove unused method

--- src/TokenWrapperFactory.sol
+++ src/TokenWrapperFactory.sol
@@ -30,21 +30,4 @@ contract TokenWrapperFactory {
 
         emit TokenWrapperDeployed(underlyingToken, unlockTime, tokenWrapper);
     }
-
-    // Returns the 3-letter month abbreviation for a given month number (1-12)
-    function _getMonthAbbreviation(uint256 month) internal pure returns (string memory) {
-        if (month == 1) return "Jan";
-        if (month == 2) return "Feb";
-        if (month == 3) return "Mar";
-        if (month == 4) return "Apr";
-        if (month == 5) return "May";
-        if (month == 6) return "Jun";
-        if (month == 7) return "Jul";
-        if (month == 8) return "Aug";
-        if (month == 9) return "Sep";
-        if (month == 10) return "Oct";
-        if (month == 11) return "Nov";
-        if (month == 12) return "Dec";
-        return "";
-    }
 }
