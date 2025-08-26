6e90d7a4c7d32b448ccf3c295b9d44f133de5f0b
Moody Salem <moody.salem@gmail.com>
2025-08-26 14:01:03 -0400

    format

--- src/TokenWrapperFactory.sol
+++ src/TokenWrapperFactory.sol
@@ -7,7 +7,9 @@ import {LibClone} from "solady/utils/LibClone.sol";
 /// @title TokenWrapperFactory - Factory for creating time-locked token wrappers
 /// @notice Creates TokenWrapper contracts with formatted names and symbols based on unlock dates
 contract TokenWrapperFactory {
-    event TokenWrapperDeployed(ERC20 underlyingToken, uint256 unlockTime, TokenWrapper tokenWrapper);
+    event TokenWrapperDeployed(
+        ERC20 underlyingToken, uint256 unlockTime, TokenWrapper tokenWrapper
+    );
 
     TokenWrapper public immutable implementation;
 
@@ -19,11 +21,16 @@ contract TokenWrapperFactory {
     /// @param underlyingToken The token to be wrapped
     /// @param unlockTime Timestamp when tokens can be unwrapped
     /// @return tokenWrapper The deployed TokenWrapper contract
-    function deployWrapper(ERC20 underlyingToken, uint256 unlockTime) external returns (TokenWrapper tokenWrapper) {
+    function deployWrapper(ERC20 underlyingToken, uint256 unlockTime)
+        external
+        returns (TokenWrapper tokenWrapper)
+    {
         bytes32 salt = keccak256(abi.encode(underlyingToken, unlockTime));
 
         tokenWrapper = TokenWrapper(
-            LibClone.cloneDeterministic(address(implementation), abi.encode(underlyingToken, unlockTime), salt)
+            LibClone.cloneDeterministic(
+                address(implementation), abi.encode(underlyingToken, unlockTime), salt
+            )
         );
 
         emit TokenWrapperDeployed(underlyingToken, unlockTime, tokenWrapper);
